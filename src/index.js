bddc477ef9c020f2eb91c21121da68eaeac31aa7
Sarffk17 <Ksarff17@gmail.com>
2024-11-02 11:30:04 -0400

    Created additional context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function displayTripDetails(response) {
+    console.log("Trip plans generated")
     new Typewriter("#travel-plans", {
-        strings: response.data.answer,
+        strings: response.data.answer
         autoStart: true, 
         delay: 1, 
         cursor: "",
@@ -10,10 +11,14 @@ function displayTripDetails(response) {
 function generateTrip(event) {
     event.preventDefault();
 
+        let instructionsInput = docume.querySelector("#user-instructions")
+
     let apiKey = "4tao58d1a814447011bb9de5e3623b3f";
-    let prompt = "";
-    let context = "";
+    let context = "You are an adventurous tourist and love to try everything that is recommended. Your mission is to generate a 3-day trip iternary with at least 6 activites in basic HTML. Make sure to follow the user instructions.";
+    let prompt = 'User Instructions: Generate a 3-day itinerary about ${instructionInput.value}';
     let apiUrl = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
+
+    console.log("Generating trip plans");
 
     axios.get(apiUrl).then(displayTripDetails);
 }
