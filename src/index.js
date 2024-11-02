9ca71b8d8d3bc24ea939f284e376fe4fcd27f70b
Sarffk17 <Ksarff17@gmail.com>
2024-11-02 12:22:03 -0400

    Updated code to dispell issues with display issues..

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 function displayTripDetails(response) {
-    new Typewriter("#travel-plans", {
-        strings: response.data.answer
-        autoStart: true, 
-        delay: 1, 
-        cursor: "",
+    new Typewriter("#travel-plan", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
     });
-}
-
-function generateTrip(event) {
+  }
+  
+  function generateTrip(event) {
     event.preventDefault();
-
-    let instructionsInput = docume.querySelector("#user-instructions")
+  
+    let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "4tao58d1a814447011bb9de5e3623b3f";
-    let context = "You are an adventurous tourist and love to try everything that is recommended. Your mission is to generate a 3-day trip iternary with at least 6 activites in basic HTML. Make sure to follow the user instructions.";
-    let prompt = `User instructions: Generate a 3-day trip intinerary about ${instructionsInput.value}`;
+    let context =
+      "You are an adventurous tourist and love to try everything that is recommended. Your mission is to generate a 3-day trip iternary with at least 6 activites in basic HTML. Make sure to follow the user instructions and separate each line with a <br />. Do not include a title to the Trip. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let prompt = `User instructions: Generate a 3-day trip itinerary poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-    let poemElement = document.querySelector("#travel-plans");
+  
+    let poemElement = document.querySelector("#travel-plan");
     poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `<div class="generating">⏳ Generating a 3-day Travel Itinerary about ${instructionsInput.value}</div>`;
-
-    axios.get(apiUrl).then(displayTripDetails);
-}
-
-let tripFormElement = document.querySelector("#travel-plans-generator");
-tripFormElement .addEventListener("submit", generateTrip);
-
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a 3-day trip itinerary about ${instructionsInput.value}</div>`;
+  
+    axios.get(apiURL).then(displayTripDetails);
+  }
+  
+  let poemFormElement = document.querySelector("#travel-plans-generator");
+  poemFormElement.addEventListener("submit", generateTrip);
