61d30450d919ab57418a55bec5e10c92b3e162f9
Sarffk17 <Ksarff17@gmail.com>
2024-11-02 11:41:10 -0400

    Used code source to enable my ${} to work.
    My ${} will not populate naturally.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayTripDetails(response) {
-    console.log("Trip plans generated")
     new Typewriter("#travel-plans", {
         strings: response.data.answer
         autoStart: true, 
@@ -11,14 +10,15 @@ function displayTripDetails(response) {
 function generateTrip(event) {
     event.preventDefault();
 
-        let instructionsInput = docume.querySelector("#user-instructions")
-
+    let instructionsInput = docume.querySelector("#user-instructions")
     let apiKey = "4tao58d1a814447011bb9de5e3623b3f";
     let context = "You are an adventurous tourist and love to try everything that is recommended. Your mission is to generate a 3-day trip iternary with at least 6 activites in basic HTML. Make sure to follow the user instructions.";
-    let prompt = 'User Instructions: Generate a 3-day itinerary about ${instructionInput.value}';
-    let apiUrl = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
+    let prompt = `User instructions: Generate a 3-day trip intinerary about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    console.log("Generating trip plans");
+    let poemElement = document.querySelector("#travel-plans");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a 3-day Travel Itinerary about ${instructionsInput.value}</div>`;
 
     axios.get(apiUrl).then(displayTripDetails);
 }
