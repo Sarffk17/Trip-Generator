4b857b93aa2d58b7dcc664bee18485cc636f32cc
Sarffk17 <Ksarff17@gmail.com>
2024-11-02 11:01:34 -0400

    Created api variable and additional function.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function generateTrip(event) {
-    event.preventDefault();
-
+function displayTripDetails(response) {
     new Typewriter("#travel-plans", {
-        strings: "3-Day Travel Plans",
+        strings: response.data.answer,
         autoStart: true, 
         delay: 1, 
         cursor: "",
     });
+}
+
+function generateTrip(event) {
+    event.preventDefault();
+
+    let apiKey = "4tao58d1a814447011bb9de5e3623b3f";
+    let prompt = "";
+    let context = "";
+    let apiUrl = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
+
+    axios.get(apiUrl).then(displayTripDetails);
 }
 
 let tripFormElement = document.querySelector("#travel-plans-generator");
